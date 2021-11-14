a6a630ca824a311141b6102561ab3f426626bb26
00wxf <1171010082@qq.com>
2021-11-14 08:58:21 -0500

    lab02

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL || head->next == NULL) 
+    return 0;
+    node *tortoise = head, *hare = head;
+    while (hare && hare->next) 
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+		if (tortoise == hare) 
+		return 1;
+	}
     return 0;
 }
